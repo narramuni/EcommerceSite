661aa362369f603c88ec76896094301b79c9ab98
narramuni <nmspavan@gmail.com>
2023-11-29 02:49:42 +0530

    First Commit

---
+++ Models/Laptop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceSite.Models
+{
+    public class Laptop
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+    }
+}
